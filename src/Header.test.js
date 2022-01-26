c1477bf070b21ecc08a06574e0a75625114c43b9
JoKalna <madara.k@hotmail.co.uk>
2022-01-26 17:34:19 +0000

    Revert "first commit of fail-pass-refacator"
    This reverts commit 7cc0f565519e0c89691dd4c80fefd83a8c8eeeb6.

--- src/Header.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Header from './Header';
-
-test('renders withouth crashing', () => {
-  render(<Header />);
-  const textElement = screen.getByText('Hello World');
-  expect(textElement).toBeInTheDocument();
-});
